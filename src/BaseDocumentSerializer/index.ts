649660c33f75481012523184f1a5d4f6df8218d6
Carolina Gonzalez <carolina.nicole.gonzalez@gmail.com>
2022-03-29 11:28:16 -0700

    separate out BaseDocumentSerializer

--- src/BaseDocumentSerializer/index.ts
+++ src/BaseDocumentSerializer/index.ts
@@ -1,9 +1,10 @@
 import blocksToHtml, { h } from '@sanity/block-content-to-html'
-import { defaultStopTypes, customSerializers } from './BaseSerializationConfig'
-import { ObjectField, SanityDocument } from '@sanity/types'
-import { Serializer, TranslationLevel } from './types'
+import { defaultStopTypes, customSerializers } from '../BaseSerializationConfig'
+import { SanityDocument } from '@sanity/types'
+import { Serializer, TranslationLevel } from '../types'
 import Schema from '@sanity/schema'
 import clone from 'just-clone'
+import { fieldFilter, languageObjectFieldFilter } from './fieldFilters'
 
 type SerializerClosure = (schemes: Schema) => Serializer
 const META_FIELDS = ['_key', '_type', '_id']
@@ -68,8 +69,9 @@ export const BaseDocumentSerializer: SerializerClosure = (schemas: Schema) => {
         if (!isFieldLevel) {
           //now we add an additional field wrapper so we know both
           //the field and type of this object after deserialization
-          //TODO: encode this with data-level: fieldname
-          serializedFields[key] = `<div class='${key}'>${serialized}</div>`
+          serializedFields[
+            key
+          ] = `<div class='${key}' data-level='field'>${serialized}</div>`
         } else {
           serializedFields[key] = serialized
         }
@@ -103,119 +105,6 @@ export const BaseDocumentSerializer: SerializerClosure = (schemas: Schema) => {
       name: doc._id,
       content: rawHTML.outerHTML,
     }
-  }
-
-  /*
-   * Helper. If field-level translation pattern used, only sends over
-   * content from the base language. Now works recursively!
-   */
-  const languageObjectFieldFilter = (
-    obj: Record<string, any>,
-    baseLang: string
-  ) => {
-    const filterToLangField = (obj: Record<string, any>) => {
-      const filteredObj: Record<string, any> = {}
-      filteredObj[baseLang] = obj[baseLang]
-      META_FIELDS.forEach(field => {
-        if (obj[field]) {
-          filteredObj[field] = obj[field]
-        }
-      })
-      return filteredObj
-    }
-
-    const findBaseLang = (
-      childObj: Record<string, any>
-    ): Record<string, any> => {
-      const filteredObj: Record<string, any> = {}
-      META_FIELDS.forEach(field => {
-        if (childObj[field]) {
-          filteredObj[field] = childObj[field]
-        }
-      })
-
-      for (let key in childObj) {
-        const value: any = childObj[key]
-        //we've reached a base language field, add it to
-        //what we want to send to translation
-        if (value.hasOwnProperty(baseLang)) {
-          filteredObj[key] = filterToLangField(value)
-        }
-        //we have an array that may have language fields in its objects
-        else if (
-          Array.isArray(value) &&
-          value.length &&
-          typeof value[0] === 'object'
-        ) {
-          //recursively find and filter for any objects that have the base language
-          const validLangObjects = value.reduce((validArr, objInArray) => {
-            if (objInArray._type === 'block') {
-              validArr.push(objInArray)
-            } else if (objInArray.hasOwnProperty(baseLang)) {
-              validArr.push(filterToLangField(objInArray))
-            } else {
-              const filtered = findBaseLang(objInArray)
-              if (Object.keys(filtered).length) {
-                validArr.push(filtered)
-              }
-            }
-            return validArr
-          }, [])
-          if (validLangObjects.length) {
-            filteredObj[key] = validLangObjects
-          }
-        }
-        //we have an object nested in an object
-        //recurse down the tree
-        else if (typeof value === 'object') {
-          const nestedLangObj = findBaseLang(value)
-          if (Object.keys(nestedLangObj).length) {
-            filteredObj[key] = nestedLangObj
-          }
-        }
-      }
-      return filteredObj
-    }
-
-    //send top level object into recursive function
-    return findBaseLang(obj)
-  }
-
-  /*
-   * Helper. Eliminates stop-types and non-localizable fields.
-   */
-  const fieldFilter = (
-    obj: Record<string, any>,
-    objFields: ObjectField[],
-    stopTypes: string[]
-  ) => {
-    const filteredObj: Record<string, any> = {}
-
-    const fieldFilter = (field: Record<string, any>) => {
-      if (field.localize === false) {
-        return false
-      } else if (field.type === 'string' || field.type === 'text') {
-        return true
-      } else if (Array.isArray(obj[field.name])) {
-        return true
-      } else if (!stopTypes.includes(field.type)) {
-        return true
-      }
-      return false
-    }
-
-    const validFields = [
-      '_key',
-      '_type',
-      '_id',
-      ...objFields.filter(fieldFilter).map(field => field.name),
-    ]
-    validFields.forEach(field => {
-      if (obj[field]) {
-        filteredObj[field] = obj[field]
-      }
-    })
-    return filteredObj
   }
 
   const serializeArray = (
