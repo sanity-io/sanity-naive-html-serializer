7ba5a1c54b9da550c412a9abe723ab9a2c05db53
Carolina Gonzalez <carolina.nicole.gonzalez@gmail.com>
2023-03-10 11:53:01 -0500

    chore: export custom block deserializers

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 export {BaseDocumentMerger} from './BaseDocumentMerger'
 export {BaseDocumentSerializer} from './BaseDocumentSerializer'
 export {BaseDocumentDeserializer} from './BaseDocumentDeserializer'
-export {defaultStopTypes, customSerializers} from './BaseSerializationConfig'
+export {
+  defaultStopTypes,
+  customSerializers,
+  customBlockDeserializers,
+} from './BaseSerializationConfig'
 
 export type {SerializedDocument, Serializer, SerializerClosure, Deserializer, Merger} from './types'
